f7208ccd65a1f72d2f0a9530e0806ea19ec154ce
hukuryo <ryohei.1998.05.09@gmail.com>
2023-03-25 13:34:33 +0900

    検索機能実装

--- index.js
+++ index.js
@@ -4,31 +4,39 @@ btn.addEventListener('click', async() => {
     const textValue = document.getElementById("formText").value;
     const res = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${textValue}`);
     const data = await res.json();
+    const bookItem = document.getElementById("bookItem");
     for(let i = 0; i < data.items.length; i++){
-        const bookTitle = data.items[i].volumeInfo.title;
-        const bookItem = document.getElementById("bookItem");
-        bookItem.innerHTML = `
+        try{
+            // JSONデータの取得
+            const bookImg = data.items[i].volumeInfo.imageLinks.smallThumbnail;
+            const makeElement = document.createElement("div");
+            const bookTitle = data.items[i].volumeInfo.title;
+            const bookContent = data.items[i].volumeInfo.description;
+            const bookLink = data.items[i].volumeInfo.infoLink;
+            makeElement.setAttribute("id", `bookItem${i}`);
+            bookItem.appendChild(makeElement);
+            const getBookItem = document.getElementById(`bookItem${i}`);
+            const setBookElement = `
                 <div class="container">
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                        <div class="card-body">
-                            <p class="card-text">sample</p>
-                            <div class="d-flex justify-content-between align-items-center">
+                    <div class="col">
+                        <div class="card shadow-sm">
+                            <div class="card-body">
+                                <img src="${bookImg}"><br>
+                                <a id="link${i}" class="card-text" target="_blank">${bookTitle}</a>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <p>${bookContent}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        `;
+            `;
+            getBookItem.innerHTML = setBookElement;
+            const link = document.getElementById(`link${i}`);
+            link.href = bookLink;
+        }catch(e){
+            console.log(e.message);
+            continue;
+        }
     }
-    // const items = data.items.map(item => {
-    //     var vi = item.volumeInfo;
-    //     return {
-    //         title: vi.title,
-    //         description: vi.description,
-    //         link: vi.infoLink,
-    //         image: vi.imageLinks ? vi.imageLinks.smallThumbnail : '',
-    //     }; 
-    // });
 })
